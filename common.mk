18b88f4229b8189c3536676eb3766631164fbc75
chandu078 <chandudyavanapelli03@gmail.com>
2020-11-01 05:15:44 +0000

    sdm845-common: don't build wfd jar

--- common.mk
+++ common.mk
@@ -243,8 +243,8 @@ PRODUCT_PACKAGES += \
     libwifi-system-iface \
     WifiOverlay
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
